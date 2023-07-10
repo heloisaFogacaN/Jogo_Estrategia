06bcebde396d41b5d9709a9f73f5614c0c3e794b
HELOISA FOGAÇA DO NASCIMENTO <heloisa_nascimento1@estudante.sesisenai.org.br>
2023-07-10 16:49:56 -0300

    Implementa movimento torre

--- src/Posicao.java
+++ src/Posicao.java
@@ -1,11 +1,20 @@
 public class Posicao {
     public boolean getUnidade;
     private Unidade unidade;
+    private Torre torre;
 
     public Unidade getUnidade() {return unidade;}
 
     public void setUnidade(Unidade unidade) {
         this.unidade = unidade;
+    }
+
+    public void setTorre(Torre torre) {
+        this.torre = torre;
+    }
+
+    public Torre getTorre() {
+        return torre;
     }
 
     @Override
