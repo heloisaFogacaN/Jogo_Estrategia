e7db3d9d6daf9c50f956ab28ef622756c92e522a
heloisaFogacaN <heloisa_nascimento1@estudante.sesisenai.org.br>
2023-07-21 13:51:09 -0300

    Implementa mais algumas coisas

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -4,26 +4,34 @@ public class Tabuleiro {
     private ArrayList<Posicao> posicoes = new ArrayList<>();
 
     Tabuleiro() {
+        ArrayList<Posicao> posicaoTorre = new ArrayList<>();
+
         for (int i = 0; i < 62; i++) {
             posicoes.add(new Posicao());
             if (i > 2 && i < 6) {
                 posicoes.get(i).setUnidade(new Torre(20, 0, 0, "branca"));
+                posicaoTorre.add(posicoes.get(i));
             }
             if (i > 18 && i < 21) {
                 posicoes.get(i).setUnidade(new Torre(20, 0, 0, "branca"));
+                posicaoTorre.add(posicoes.get(i));
             }
             if (i > 23 && i < 26) {
                 posicoes.get(i).setUnidade(new Torre(20, 0, 0, "branca"));
+                posicaoTorre.add(posicoes.get(i));
             }
             if (i > 36 && i < 39) {
                 posicoes.get(i).setUnidade(new Torre(20, 0, 0, "preta"));
+                posicaoTorre.add(posicoes.get(i));
             }
             if (i > 41 && i < 44) {
                 posicoes.get(i).setUnidade(new Torre(20, 0, 0, "preta"));
+                posicaoTorre.add(posicoes.get(i));
             }
-                if (i > 56 && i < 60) {
-                    posicoes.get(i).setUnidade(new Torre(20, 0, 0, "preta"));
-                }
+            if (i > 56 && i < 60) {
+                posicoes.get(i).setUnidade(new Torre(20, 0, 0, "preta"));
+                posicaoTorre.add(posicoes.get(i));
+            }
         }
     }
     public ArrayList<Posicao> getPosicoes() {
